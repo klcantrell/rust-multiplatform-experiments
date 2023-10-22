bd00477973789c033792cbedeaea5583659f8476
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-10-21 21:39:36 -0400

    Example of using Rust + Uniffi to create a iOS static library (directly linked)

---
+++ TryUniffiRust/TryUniffiRust/ContentView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            print("4 + 5 = \(add(4, 5))")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TryUniffiRust/TryUniffiRust/TryUniffiRustApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryUniffiRustApp.swift
+//  TryUniffiRust
+//
+//  Created by Kalalau L. Cantrell on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TryUniffiRustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
